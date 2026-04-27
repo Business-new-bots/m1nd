94e6718778904744367c52b23ef07d825806ad4a
Pavel Bukhtsiyarau <p.bukhtsiyarau@gmail.com>
2026-04-27 10:04:37 +0300

    tracker with pattern

--- src/main/java/com/example/m1nd/repository/HabitDailyTaskRepository.java
+++ src/main/java/com/example/m1nd/repository/HabitDailyTaskRepository.java
@@ -2,7 +2,9 @@ package com.example.m1nd.repository;
 
 import com.example.m1nd.model.HabitDailyTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,8 +12,14 @@ import java.util.Optional;
 
 @Repository
 public interface HabitDailyTaskRepository extends JpaRepository<HabitDailyTask, Long> {
+    @Modifying
+    @Transactional
     void deleteByTaskDateBefore(LocalDate date);
+
+    @Modifying
+    @Transactional
     void deleteByUserIdAndTaskDate(Long userId, LocalDate taskDate);
+
     Optional<HabitDailyTask> findTopByUserIdAndTaskDateOrderByCreatedAtDesc(Long userId, LocalDate taskDate);
     List<HabitDailyTask> findByTaskDateAndRemindEveningTrueAndEveningReminderSentAtIsNull(LocalDate taskDate);
 }
